1d62cb975f7b84bbc2df1510c7a90e23656fbf2c
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2021-08-09 21:57:48 +0500

    Add controller logic with http request client

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return 'Hello';
+  }
+
+  @Get(':id')
+  getMessage(@Param('id') id: number) {
+    console.log(id);
+  }
+
+  @Post()
+  createMessage(@Body() body: any) {
+    console.log(body);
+  }
+}
