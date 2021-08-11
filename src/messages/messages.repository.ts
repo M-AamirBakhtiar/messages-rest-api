9a6ad5bce3807c8c3cead2f8ae8a483eaf2adece
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2021-08-11 23:43:06 +0500

    Add find messages by id

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,7 +1,11 @@
 import { readFile } from 'fs/promises';
 
 export class MessagesRepository {
-  async findOne(id: string) {}
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
 
   async findAll() {}
 
