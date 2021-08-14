fdbe0133fe79d455f6e20f1e1fd20c83f05d1382
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2021-08-14 21:57:00 +0500

    Add services logic and injection

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
+import { MessageService } from './messages.service';
 
 @Module({
   controllers: [MessagesController],
+  providers: [MessageService],
 })
 export class MessagesModule {}
