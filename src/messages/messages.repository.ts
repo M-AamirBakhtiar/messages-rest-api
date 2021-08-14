e8a0bbaa933533fd171c8b6d890a38c12d4e8565
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2021-08-14 21:44:13 +0500

    Add read and write to json

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 
 export class MessagesRepository {
   async findOne(id: string) {
@@ -7,7 +7,20 @@ export class MessagesRepository {
     return messages[id];
   }
 
-  async findAll() {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
 
-  async create() {}
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
